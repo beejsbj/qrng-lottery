34f6011831e2216ff87a8ae64351e9245e81a2c9
beejsbj <burooj.bj@gmail.com>
2023-03-03 20:43:20 -0500

    Connect reset store to button

--- frontend/src/components/LotteryApp/ResetLottery.jsx
+++ frontend/src/components/LotteryApp/ResetLottery.jsx
@@ -3,13 +3,21 @@ import useStore from "../../store";
 export default function ResetLottery() {
   const nbsp = "\u00A0";
   const hasLotteryEnded = useStore((state) => state.hasLotteryEnded);
+  const resetLottery = useStore((state) => state.reset.writeContract);
+
+  async function handleReset(event) {
+    event.preventDefault();
+    await resetLottery();
+  }
 
   return (
     <div className={`${hasLotteryEnded ? "reset-lottery" : "hide"}`}>
       <h2 className="attention-voice">
         Lottery{nbsp}has{nbsp}ended
       </h2>
-      <button className="button">Reset{nbsp}Lottery</button>
+      <button onClick={handleReset} className="button">
+        Reset{nbsp}Lottery
+      </button>
     </div>
   );
 }
