aa42c0d2c8605854940e99767750576cc2b481ff
beejsbj <burooj.bj@gmail.com>
2023-03-05 20:08:33 -0500

    Frontend Finished

--- frontend/src/components/LotteryApp/PastWinners.jsx
+++ frontend/src/components/LotteryApp/PastWinners.jsx
@@ -1,22 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useStore from "../../store";
 
 export default function PastWinners() {
   const [winners, setWinners] = useState([335, 500, 1100.11]);
-  const getWinner = useStore((state) => state.winner.getLastWeekWinningNumber);
-  const winner = useStore((state) => state.winner.lastWeekWinningNumber);
-  getWinner();
-  console.log(winner);
+
+  //winning number of last N weeks
+  const getWinningNumbers = useStore(
+    (state) => state.winner.getLastNWeeksWinningNumber
+  );
+  const WinningNumbers = useStore(
+    (state) => state.winner.LastNWeeksWinningNumber
+  );
+
+  //winning pot of last N weeks
+  const getWinningPots = useStore(
+    (state) => state.winner.getLastNWeeksWinningPot
+  );
+  const WinningPots = useStore((state) => state.winner.LastNweeksWinningPot);
+
+  useEffect(() => {
+    getWinningNumbers();
+    getWinningPots();
+  }, []);
+
   return (
     <past-card class="slide-in-left">
       <h2 className="teaser-voice">past winners</h2>
 
       <ol>
-        {winners.map((winner, i) => {
+        {WinningNumbers.map((WinningNumber, i) => {
+          if (WinningNumber.length == 1) {
+            WinningNumber = "0" + WinningNumber;
+          }
           return (
-            <li key={i} className={i == 0 ? "loud-voice" : "attention-voice"}>
-              {" "}
-              {winner}{" "}
+            <li
+              key={i}
+              className={i == 0 ? "attention-voice" : "attention-voice"}
+            >
+              {WinningNumber} ⇒ Ξ {WinningPots[i]}
             </li>
           );
         })}
--- frontend/src/store/winner.jsx
+++ frontend/src/store/winner.jsx
@@ -1,20 +1,43 @@
 import { readContract, writeContract } from "@wagmi/core";
-import { BigNumber } from "ethers";
+import { BigNumber, ethers } from "ethers";
 import tokenContract from "../contracts/lottery.json";
 
 export const winner = (set, get) => ({
-  lastWeekWinningNumber: 0,
-  getLastWeekWinningNumber: async () => {
+  LastNWeeksWinningNumber: [],
+  LastNweeksWinningPot: [],
+
+  getLastNWeeksWinningNumber: async () => {
     const data = await readContract({
       abi: tokenContract,
       address: "0x690B73FD0A7f922802C4E79f2465fd86C78b2Eee",
-      functionName: "getLastWeekWinningNumber",
+      functionName: "getLastNWeeksWinningNumber",
+      args: [3],
     });
     set((state) => ({
       ...state,
       winner: {
         ...state.winner,
-        lastWeekWinningNumber: BigNumber.from(data).toNumber(),
+        LastNWeeksWinningNumber: data.map((number) =>
+          BigNumber.from(number).toString()
+        ),
+      },
+    }));
+  },
+
+  getLastNWeeksWinningPot: async () => {
+    const data = await readContract({
+      abi: tokenContract,
+      address: "0x690B73FD0A7f922802C4E79f2465fd86C78b2Eee",
+      functionName: "getLastNWeeksWinningPot",
+      args: [3],
+    });
+    set((state) => ({
+      ...state,
+      winner: {
+        ...state.winner,
+        LastNweeksWinningPot: data.map((number) =>
+          ethers.utils.formatEther(BigNumber.from(number))
+        ),
       },
     }));
   },
